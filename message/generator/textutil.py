483bc194bcce9557c6a78a3137aa9184ee1bdce5
Trent Houliston <trent@houliston.me>
2016-10-04 15:23:47 +1100

    Squashed 'nuclear/' changes from 8489d78..d940248
    d940248 Refactor mission transmit to drone to be more reliable
    378ed69 Revert "Add a secondary constructor that takes values by copy instead of const reference to help with parsing packed structs"
    cc08d11 Add a secondary constructor that takes values by copy instead of const reference to help with parsing packed structs
    948b128 Added in the ability to declare fixed sized arrays
    03a90ec Add the toolchain library finder to the NUClear roles system.
    71ee388 Remove forcing protobufs to be camel case in enhanced messages
    
    git-subtree-dir: nuclear
    git-subtree-split: d94024889a77ec5f18330da803127ded6453a12a

--- message/generator/textutil.py
+++ message/generator/textutil.py
@@ -7,10 +7,3 @@ def indent(str, len=4):
 
 def dedent(str):
     return textwrap.dedent(str)
-
-
-def to_camel_case(snake_str):
-    components = snake_str.split('_')
-    # We capitalize the first letter of each component except the first one
-    # with the 'title' method and join them together.
-    return components[0] + "".join(x.title() for x in components[1:])
