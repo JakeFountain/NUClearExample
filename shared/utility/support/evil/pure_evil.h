1ce424a935a035df20bbc6d8018bd2bcd4194dd9
Trent Houliston <trent@houliston.me>
2018-12-16 11:24:48 +1100

    Update many components

--- shared/utility/support/evil/pure_evil.h
+++ shared/utility/support/evil/pure_evil.h
@@ -18,42 +18,35 @@
 #ifndef UTILITY_SUPPORT_EVIL_PUREEVIL_H
 #define UTILITY_SUPPORT_EVIL_PUREEVIL_H
 
-#include <vector>
 #include <string>
+#include <vector>
 
 // If we are not in debug mode, don't build it! Please don't build it!
-#if !defined(NDEBUG) && !defined(__APPLE__)
+#if !defined(NDEBUG) and !defined(__APPLE__)
 
 namespace utility {
-    namespace support {
-        namespace evil {
+namespace support {
+    namespace evil {
 
-            struct StackFrame {
+        struct StackFrame {
 
-                StackFrame()
-                : pc()
-                , file()
-                , lineno()
-                , function() {}
+            StackFrame() : pc(), file(), lineno(), function() {}
 
-                StackFrame(uintptr_t pc, std::string file, int lineno, std::string function)
-                : pc(pc)
-                , file(file)
-                , lineno(lineno)
-                , function(function) {}
+            StackFrame(uintptr_t pc, std::string file, int lineno, std::string function)
+                : pc(pc), file(file), lineno(lineno), function(function) {}
 
-                uintptr_t pc;
-                std::string file;
-                int lineno;
-                std::string function;
-            };
+            uintptr_t pc;
+            std::string file;
+            int lineno;
+            std::string function;
+        };
 
-            thread_local std::vector<StackFrame> stack;
-            thread_local std::string exception_name;
-        }
-    }
-}
+        extern thread_local std::vector<StackFrame> stack;
+        extern thread_local std::string exception_name;
+    }  // namespace evil
+}  // namespace support
+}  // namespace utility
 
-#endif  // NDEBUG or APPLE
+#endif  // !defined(NDEBUG) and !defined(__APPLE__)
 
 #endif  // UTILITY_SUPPORT_EVIL_PUREEVIL_H
